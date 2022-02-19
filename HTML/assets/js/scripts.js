bfaef68ba8d0bbe6b42e422f9ebdfbeaec03caf6
Prakash Anandan <prakasha@gmail.com>
2022-02-19 21:53:17 +0530

    Removed twitter and map sections

--- HTML/assets/js/scripts.js
+++ HTML/assets/js/scripts.js
@@ -236,44 +236,7 @@ $(document).ready(function() {
 	$('.close-preview').click(function(){
 		closeProject();
 	})
-	
-	/*============================================
-	Twitter
-	==============================================*/
-	var tweetsLength = $('#twitter-slider').data('tweets-length'),
-		widgetID = $('#twitter-slider').data('widget-id');
-	
-	twitterFetcher.fetch(widgetID, 'twitter-slider', tweetsLength, true, false, true, '', false, handleTweets);
 
-	function handleTweets(tweets){
-	
-		var x = tweets.length,
-			n = 0,
-			tweetsHtml = '<ul class="slides">';
-			
-		while(n < x) {
-			tweetsHtml += '<li>' + tweets[n] + '</li>';
-			n++;
-		}
-		
-		tweetsHtml += '</ul>';
-		$('#twitter-slider').html(tweetsHtml);
-	
-		$('.twitter_reply_icon').html("<i class='fa fa-reply'></i>");
-		$('.twitter_retweet_icon').html("<i class='fa fa-retweet'></i>");
-		$('.twitter_fav_icon').html("<i class='fa fa-star'></i>");
-	  
-		$('#twitter-slider').flexslider({
-			prevText: '<i class="fa fa-angle-left"></i>',
-			nextText: '<i class="fa fa-angle-right"></i>',
-			slideshowSpeed: 5000,
-			useCSS: true,
-			controlNav: false, 
-			pauseOnAction: false, 
-			pauseOnHover: true,
-			smoothHeight: false
-		});
-	}
 	/*============================================
 	Contact Map
 	==============================================*/
